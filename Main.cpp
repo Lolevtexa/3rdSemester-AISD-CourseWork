bd3b8a8ff5a6c8505ce9288148542bc2c75e1f3a
Nikolaev Vsevolod <ceba.kek@gmail.com>
2023-12-24 19:22:18 +0300

    Refactor SandPile class and add color gradient

--- Main.cpp
+++ Main.cpp
@@ -9,8 +9,14 @@ const int GRID_WIDTH = WIDTH / CELL_SIZE;
 const int GRID_HEIGHT = HEIGHT / CELL_SIZE;
 
 class SandPile {
-protected:
+private:
     std::vector<std::vector<int>> grid;
+
+    static const sf::Color background;
+    static const sf::Color outline;
+    static const sf::Color green;
+    static const sf::Color purple;
+    static const sf::Color gold;
 
 public:
     SandPile() {
@@ -19,14 +25,6 @@ public:
 
     void addSand(int x, int y) {
         grid[x][y]++;
-    }
-
-    void topple(int x, int y) {
-        grid[x][y] -= 4;
-        if (y - 1 >= 0) grid[x][y - 1]++;
-        if (y + 1 < GRID_HEIGHT) grid[x][y + 1]++;
-        if (x - 1 >= 0) grid[x - 1][y]++;
-        if (x + 1 < GRID_WIDTH) grid[x + 1][y]++;
     }
 
     void updateGrid() {
@@ -44,21 +42,63 @@ public:
             for (int y = 0; y < GRID_HEIGHT; y++) {
                 sf::RectangleShape cell(sf::Vector2f(CELL_SIZE, CELL_SIZE));
                 cell.setPosition(x * CELL_SIZE, y * CELL_SIZE);
-                cell.setFillColor(sf::Color::White);
-                cell.setOutlineColor(sf::Color::Black);
+                cell.setFillColor(background);
+                cell.setOutlineColor(outline);
                 cell.setOutlineThickness(1.0f);
                 window.draw(cell);
 
                 if (grid[x][y] > 0) {
                     sf::CircleShape sand(CELL_SIZE / 2);
                     sand.setPosition(x * CELL_SIZE, y * CELL_SIZE);
-                    sand.setFillColor(sf::Color::Yellow);
+                    sand.setFillColor(grad(grid[x][y]));
                     window.draw(sand);
                 }
             }
         }
     }
+
+private:
+    void topple(int x, int y) {
+        grid[x][y] -= 4;
+        if (y - 1 >= 0) grid[x][y - 1]++;
+        if (y + 1 < GRID_HEIGHT) grid[x][y + 1]++;
+        if (x - 1 >= 0) grid[x - 1][y]++;
+        if (x + 1 < GRID_WIDTH) grid[x + 1][y]++;
+    }
+
+    sf::Color grad(int value) {
+        sf::Color color;
+        switch (value) {
+        case 0:
+            color = background;
+            break;
+
+        case 1:
+            color = green;
+            break;
+
+        case 2:
+            color = gold;
+            break;
+
+        case 3:
+            color = purple;
+            break;
+        
+        default:
+            color = outline;
+            break;
+        }
+
+        return color;
+    }
 };
+
+const sf::Color SandPile::background = sf::Color(255, 255, 255);
+const sf::Color SandPile::outline = sf::Color(0, 0, 0);
+const sf::Color SandPile::green = sf::Color(0, 255, 0);
+const sf::Color SandPile::purple = sf::Color(255, 0, 255);
+const sf::Color SandPile::gold = sf::Color(255, 215, 0);
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Sandpile Model");
